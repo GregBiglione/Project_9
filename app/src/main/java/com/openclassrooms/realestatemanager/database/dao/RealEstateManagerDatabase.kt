09e05423ae755a97e3bf0d424ce50a7515df8dd1
GregBiglione <gregory.biglione81@gmail.com>
2020-11-24 19:01:44 +0100

    Added Database

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/database/dao/RealEstateManagerDatabase.kt
@@ -0,0 +1,36 @@
+package com.openclassrooms.realestatemanager.database.dao
+
+import android.content.Context
+import android.util.Property
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Property::class], version = 1, exportSchema = false)
+abstract class RealEstateManagerDatabase: RoomDatabase() {
+
+    //------------------- DAO ----------------------------------------------------------------------
+    abstract fun propertyDao(): PropertyDao
+
+    //------------------- Singleton ----------------------------------------------------------------
+    companion object{
+        @Volatile
+        private var INSTANCE: RealEstateManagerDatabase? = null
+    }
+
+    fun getDatabase(context: Context): RealEstateManagerDatabase{
+        val tempInstance = INSTANCE
+        if (tempInstance != null){
+            return tempInstance
+        }
+        synchronized(this){
+            val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    RealEstateManagerDatabase::class.java,
+                    "property_database"
+            ).build()
+            INSTANCE = instance
+            return instance
+        }
+    }
+}
