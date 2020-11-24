078240313a187274a6a05a1a6f4a5d188d120bec
GregBiglione <gregory.biglione81@gmail.com>
2020-11-24 20:18:19 +0100

    Added ViewModel

--- app/src/main/java/com/openclassrooms/realestatemanager/database/dao/RealEstateManagerDatabase.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/database/dao/RealEstateManagerDatabase.kt
@@ -16,21 +16,21 @@ abstract class RealEstateManagerDatabase: RoomDatabase() {
     companion object{
         @Volatile
         private var INSTANCE: RealEstateManagerDatabase? = null
-    }
 
-    fun getDatabase(context: Context): RealEstateManagerDatabase{
-        val tempInstance = INSTANCE
-        if (tempInstance != null){
-            return tempInstance
-        }
-        synchronized(this){
-            val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    RealEstateManagerDatabase::class.java,
-                    "property_database"
-            ).build()
-            INSTANCE = instance
-            return instance
+        fun getDatabase(context: Context): RealEstateManagerDatabase{
+            val tempInstance = INSTANCE
+            if (tempInstance != null){
+                return tempInstance
+            }
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        RealEstateManagerDatabase::class.java,
+                        "property_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
         }
     }
 }
---
+++ app/src/main/java/com/openclassrooms/realestatemanager/viewmodel/PropertyViewModel.kt
@@ -0,0 +1,35 @@
+package com.openclassrooms.realestatemanager.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.openclassrooms.realestatemanager.database.dao.RealEstateManagerDatabase
+import com.openclassrooms.realestatemanager.model.Property
+import com.openclassrooms.realestatemanager.repository.PropertyRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class PropertyViewModel(application: Application): AndroidViewModel(application) {
+
+    private val getAllProperties: LiveData<List<Property>>
+    private val propertyRepository: PropertyRepository
+
+    init{
+        val propertyDao = RealEstateManagerDatabase.getDatabase(application).propertyDao()
+        propertyRepository = PropertyRepository(propertyDao)
+        getAllProperties = propertyRepository.getAllProperties
+    }
+
+    fun createProperty(property: Property){
+        viewModelScope.launch(Dispatchers.IO) {
+            propertyRepository.createProperty(property)
+        }
+    }
+
+    fun updateProperty(property: Property){
+        viewModelScope.launch(Dispatchers.IO) {
+            propertyRepository.updateProperty(property)
+        }
+    }
+}
