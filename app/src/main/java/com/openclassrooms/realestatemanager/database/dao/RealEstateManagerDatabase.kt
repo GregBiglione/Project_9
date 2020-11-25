037071163dabdf7bae3095ba667f7890568f4371
GregBiglione <gregory.biglione81@gmail.com>
2020-11-25 15:32:45 +0100

    Added prepopulate Database

--- app/src/main/java/com/openclassrooms/realestatemanager/database/dao/RealEstateManagerDatabase.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/database/dao/RealEstateManagerDatabase.kt
@@ -1,12 +1,14 @@
 package com.openclassrooms.realestatemanager.database.dao
 
 import android.content.Context
-import android.util.Property
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.openclassrooms.realestatemanager.model.Property
+import com.openclassrooms.realestatemanager.model.PropertyDataSource
 
-@Database(entities = [Property::class], version = 1, exportSchema = false)
+@Database(entities = [PropertyDataSource::class], version = 1, exportSchema = false)
 abstract class RealEstateManagerDatabase: RoomDatabase() {
 
     //------------------- DAO ----------------------------------------------------------------------
@@ -27,9 +29,26 @@ abstract class RealEstateManagerDatabase: RoomDatabase() {
                         context.applicationContext,
                         RealEstateManagerDatabase::class.java,
                         "property_database"
-                ).build()
+                )
+                        .addCallback(prepopulate(context))
+                        .build()
                 INSTANCE = instance
                 return instance
+            }
+        }
+
+        private fun prepopulate(context: Context): Callback {
+            return object: Callback(){
+                override fun onCreate(db: SupportSQLiteDatabase) {
+                    super.onCreate(db)
+                    val propertyDao = getDatabase(context).propertyDao()
+
+                    //--------- Properties ---------------------------------------------------------
+                    val properties: ArrayList<Property> = PropertyDataSource.createPropertyDataSet()
+                    for(p in properties){
+                        propertyDao.createProperty(p)
+                    }
+                }
             }
         }
     }
