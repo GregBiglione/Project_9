440455ea7834817cc502489b795996c1d2268612
GregBiglione <gregory.biglione81@gmail.com>
2020-11-29 21:30:22 +0100

    Added Agent Repository

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/repositories/AgentRepository.kt
@@ -0,0 +1,22 @@
+package com.openclassrooms.realestatemanager.repositories
+
+import com.openclassrooms.realestatemanager.database.dao.AgentDao
+import com.openclassrooms.realestatemanager.model.Agent
+
+class AgentRepository(private val agentDao: AgentDao) {
+
+    val getAllAgents = agentDao.getAllAgents()
+
+    fun createAgent(agent: Agent){
+        agentDao.createAgent(agent)
+    }
+
+    fun updateAgent(agent: Agent){
+        agentDao.updateAgent(agent)
+    }
+
+    fun deleteAgent(agent: Agent){
+        agentDao.deleteAgent(agent)
+    }
+}
+
