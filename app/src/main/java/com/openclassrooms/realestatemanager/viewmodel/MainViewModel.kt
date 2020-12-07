f0b94db75285c110e35e12cf6c0f0290ddf3ebc0
GregBiglione <gregory.biglione81@gmail.com>
2020-12-07 23:38:33 +0100

    Added add agent in Database

--- app/src/main/java/com/openclassrooms/realestatemanager/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/viewmodel/MainViewModel.kt
@@ -6,6 +6,7 @@ import com.openclassrooms.realestatemanager.model.Agent
 import com.openclassrooms.realestatemanager.model.House
 import com.openclassrooms.realestatemanager.repositories.AgentRepository
 import com.openclassrooms.realestatemanager.repositories.HouseRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainViewModel(private val agentRepository: AgentRepository, private val houseRepository: HouseRepository) : ViewModel() {
@@ -14,18 +15,18 @@ class MainViewModel(private val agentRepository: AgentRepository, private val ho
     val houses = houseRepository.getAllHouses
 
     //------------------- Agents -------------------------------------------------------------------
-    fun getAgent(id: Long) = viewModelScope.launch { agentRepository.getAgent(id) }
+    fun getAgent(id: Long) = viewModelScope.launch(Dispatchers.IO) { agentRepository.getAgent(id) }
 
-    fun createAgent(agent: Agent) = viewModelScope.launch { agentRepository.createAgent(agent) }
+    fun createAgent(agent: Agent) = viewModelScope.launch(Dispatchers.IO){ agentRepository.createAgent(agent)}
 
-    fun updateAgent(agent: Agent) = viewModelScope.launch { agentRepository.updateAgent(agent) }
+    fun updateAgent(agent: Agent) = viewModelScope.launch(Dispatchers.IO) { agentRepository.updateAgent(agent) }
 
-    fun deleteAgent(agent: Agent) = viewModelScope.launch { agentRepository.deleteAgent(agent) }
+    fun deleteAgent(agent: Agent) = viewModelScope.launch(Dispatchers.IO) { agentRepository.deleteAgent(agent) }
 
     //------------------- Houses ---------------------------------------------------------------
-    fun getHouse(id: Long) = viewModelScope.launch { houseRepository.getHouse(id) }
+    fun getHouse(id: Long) = viewModelScope.launch(Dispatchers.IO) { houseRepository.getHouse(id) }
 
-    fun createHouse(house: House) = viewModelScope.launch { houseRepository.createHouse(house) }
+    fun createHouse(house: House) = viewModelScope.launch(Dispatchers.IO) { houseRepository.createHouse(house) }
 
-    fun updateHouse(house: House) = viewModelScope.launch {  houseRepository.updateHouse(house) }
+    fun updateHouse(house: House) = viewModelScope.launch(Dispatchers.IO) {  houseRepository.updateHouse(house) }
 }
