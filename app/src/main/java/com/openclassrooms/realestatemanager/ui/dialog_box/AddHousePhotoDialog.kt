81481963d6870d198320d37c22d2da80d6ba5cf6
GregBiglione <gregory.biglione81@gmail.com>
2020-12-16 09:31:30 +0100

    Added hide keyboard when AddHousePhotoDialog's TextInputEditText not focused

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/dialog_box/AddHousePhotoDialog.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/dialog_box/AddHousePhotoDialog.kt
@@ -17,6 +17,7 @@ class AddHousePhotoDialog(context: Context): Dialog(context) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.add_house_photo_dialog)
+        hideKeyboardWhenNotFocused()
     }
 
     //----------------------------------------------------------------------------------------------
@@ -26,5 +27,18 @@ class AddHousePhotoDialog(context: Context): Dialog(context) {
     private fun closeKeyboard(view: View){
         val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    //----------------------------------------------------------------------------------------------
+    //------------------- Hide keyboard when add description TextInputEditText not focused ---------
+    //----------------------------------------------------------------------------------------------
+
+    private fun hideKeyboardWhenNotFocused(){
+        addHousePhotoDescription = findViewById(R.id.dialog_add_house_description_et)
+        addHousePhotoDescription.setOnFocusChangeListener { v, hasFocus ->
+            if (!hasFocus){
+                closeKeyboard(addHousePhotoDescription)
+            }
+        }
     }
 }
