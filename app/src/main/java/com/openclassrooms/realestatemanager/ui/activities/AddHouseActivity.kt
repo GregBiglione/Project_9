2875a9ac9c8f69ff06be5dfdf2b9e6fc57cb094f
GregBiglione <gregory.biglione81@gmail.com>
2020-12-17 05:10:19 +0100

    Added add house photo from gallery

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/activities/AddHouseActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/activities/AddHouseActivity.kt
@@ -1,12 +1,18 @@
 package com.openclassrooms.realestatemanager.ui.activities
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.droidman.ktoasty.showSuccessToast
+import com.droidman.ktoasty.showWarningToast
 import com.openclassrooms.realestatemanager.R
 import com.openclassrooms.realestatemanager.adapters.HousePhotoAdapter
 
@@ -15,6 +21,9 @@ class AddHouseActivity : AppCompatActivity() {
     private lateinit var housePhotoRecyclerView: RecyclerView
     private lateinit var housePhotoAdapter: HousePhotoAdapter
     private lateinit var housePhoto: ImageView
+    //------------------- Photo from gallery code --------------------------------------------------
+    private val IMAGE_PICK_CODE = 2108
+    private val IMAGE_PERMISSION_CODE = 1201
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,18 +46,71 @@ class AddHouseActivity : AppCompatActivity() {
     }
 
     //----------------------------------------------------------------------------------------------
-    //-------------------------------- Show house photo dialog box ---------------------------------
+    //------------------- Add house photo from gallery ---------------------------------------------
     //----------------------------------------------------------------------------------------------
 
-    fun clickOnAddHouseButton(){
+    private fun clickOnAddHouseButton(){
         housePhoto = findViewById(R.id.add_house_photo)
-        housePhoto.setOnClickListener {showSuccessToast("Click on add photo icon", Toast.LENGTH_SHORT) }
+        housePhoto.setOnClickListener {
+            checkPermission()
+            showSuccessToast("Click on add photo icon", Toast.LENGTH_SHORT) }
     }
 
-    //private fun showHousePhotoDialogBox() {
-    //    AddHousePhotoDialog(this).show()
-    //}
+    //------------------- Check permission ---------------------------------------------------------
 
+    private fun checkPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if (ActivityCompat.checkSelfPermission(application, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_DENIED){
+                // Permission denied
+                val permissions = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                // Show popup permission request
+                requestPermissions(permissions, IMAGE_PERMISSION_CODE)
+            }
+            else{
+                // Permission already granted
+                pickHousePhotoFromGallery()
+            }
+        }
+        else{
+            // System OS < Marshmallow
+            pickHousePhotoFromGallery()
+        }
+    }
+
+    //------------------- Handle permission result -----------------------------------
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when(requestCode){
+            IMAGE_PERMISSION_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    // Permission from popup granted
+                    pickHousePhotoFromGallery()
+                }
+                else{
+                    // Permission from popup denied
+                    applicationContext.showWarningToast("Permission denied", Toast.LENGTH_SHORT, true)
+                }
+            }
+        }
+    }
+
+    //------------------- Intent to access gallery -------------------------------------------------
+
+    private fun pickHousePhotoFromGallery() {
+        val accessGallery = Intent(Intent.ACTION_PICK)
+        accessGallery.type ="image/*"
+        startActivityForResult(accessGallery, IMAGE_PICK_CODE)
+    }
+
+    //------------------- Handle image pick result -----------------------------------
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+            housePhoto.setImageURI(data?.data)
+        }
+    }
 
     //----------------------------------------------------------------------------------------------
     //-------------------------------- House type spinner ------------------------------------------
