585e390dc25a1833af01e04030a3d0d190aa8080
GregBiglione <gregory.biglione81@gmail.com>
2020-12-19 11:32:39 +0100

    Added status spinner in AddHouseActivity

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/activities/AddHouseActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/activities/AddHouseActivity.kt
@@ -41,6 +41,20 @@ class AddHouseActivity : AppCompatActivity() {
     private val IMAGE_PERMISSION_CODE = 1201
     //------------------- Button -------------------------------------------------------------------
     private lateinit var addHousePhotoButton: Button
+    //------------------- Edit text ----------------------------------------------------------------
+    private lateinit var houseDescriptionEditText: TextInputEditText
+    private lateinit var houseAddressEditText: TextInputEditText
+    private lateinit var housePriceEditText: TextInputEditText
+    private lateinit var houseSurfaceEditText: TextInputEditText
+    private lateinit var houseRoomsEditText: TextInputEditText
+    private lateinit var houseBathRoomsEditText: TextInputEditText
+    private lateinit var houseBedRoomsEditText: TextInputEditText
+    private lateinit var houseSaleDate: TextInputEditText
+    //------------------- Spinner ------------------------------------------------------------------
+    private lateinit var houseTypeSpinner: Spinner
+    private lateinit var neighborSpinner: Spinner
+    private lateinit var statusSpinner: Spinner
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,6 +66,7 @@ class AddHouseActivity : AppCompatActivity() {
         clickOnAddHousePhotoButton()
         typeSpinner()
         neighborhoodSpinner()
+        statusSpinner()
     }
 
     //----------------------------------------------------------------------------------------------
@@ -215,7 +230,7 @@ class AddHouseActivity : AppCompatActivity() {
     //-------------------------------- House type spinner ------------------------------------------
     //----------------------------------------------------------------------------------------------
 
-    private fun typeSpinner(){
+     private fun typeSpinner(){
         val houseType = resources.getStringArray(R.array.house_type)
         val houseTypeSpinner = findViewById<Spinner>(R.id.add_house_type_spinner)
         if (houseTypeSpinner != null){
@@ -253,5 +268,62 @@ class AddHouseActivity : AppCompatActivity() {
                 override fun onNothingSelected(parent: AdapterView<*>?) {}
             }
         }
+    }
+
+    //----------------------------------------------------------------------------------------------
+    //-------------------------------- Status spinner ----------------------------------------------
+    //----------------------------------------------------------------------------------------------
+
+    private fun statusSpinner(){
+        val houseStatus = resources.getStringArray(R.array.house_status_array)
+        val statusSpinner = findViewById<Spinner>(R.id.add_house_status_spinner)
+        if (statusSpinner != null){
+            val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, houseStatus)
+            statusSpinner.adapter = adapter
+
+            statusSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    val statusSelected: String = statusSpinner.selectedItem.toString().trim()
+                    showSuccessToast("Status: $statusSelected", Toast.LENGTH_SHORT)
+                    //val item = statusSpinner.getItemAtPosition(1)
+                    //if (item == 1){
+                    //    //houseSaleDate
+                    //    showWarningToast("House is: $item", Toast.LENGTH_SHORT)
+                    //}
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
+            }
+        }
+    }
+
+    //----------------------------------------------------------------------------------------------
+    //-------------------------------- Add house in room db ----------------------------------------
+    //----------------------------------------------------------------------------------------------
+
+    private fun saveHouse(){
+        // house photos uses housePhoto getAllHousePhotos
+
+        houseDescriptionEditText = findViewById(R.id.add_house_description)
+        houseAddressEditText = findViewById(R.id.add_house_address)
+        housePriceEditText = findViewById(R.id.add_house_price)
+        houseSurfaceEditText = findViewById(R.id.add_house_surface)
+        houseRoomsEditText = findViewById(R.id.add_house_number_of_rooms)
+        houseBathRoomsEditText = findViewById(R.id.add_house_number_of_bathrooms)
+        houseBedRoomsEditText = findViewById(R.id.add_house_number_of_bedrooms)
+
+        val id: Long = System.currentTimeMillis()
+        //var housePhotoList: List<HousePhoto> = mainViewModel.allHousePhotos
+        val typeHouseSelected: String = houseTypeSpinner.selectedItem.toString().trim()
+        val neighborhoodSelected: String = neighborSpinner.selectedItem.toString().trim()
+        var houseDescription: String = housePhotoDescriptionEditText.text.toString().trim()
+        var houseAddress: String = houseAddressEditText.text.toString().trim()
+        var houseSurface: String = houseSurfaceEditText.text.toString().trim()
+        var houseRooms: String = houseRoomsEditText.text. toString().trim()
+        var houseBathRooms: String = houseBathRoomsEditText.text. toString().trim()
+        var houseBedRooms: String = houseBedRoomsEditText.text. toString().trim()
+        val statusSelected: String = statusSpinner.selectedItem.toString().trim()
+        // set entry date with Today date
+        // Set sale date with Date picker
     }
 }
