9ec2bd86ef1e0c316ec8d9336c3b547653ded666
GregBiglione <gregory.biglione81@gmail.com>
2021-01-06 15:58:01 +0100

    Added share preferences in SettingsFragment & root preferences improvement

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/fragments/SettingsFragment.kt
@@ -1,21 +1,43 @@
 package com.openclassrooms.realestatemanager.ui.fragments
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 import com.openclassrooms.realestatemanager.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
+    private val NOTIFICATIONS_PREF = "Notifications preferences"
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        loadSettings()
     }
 
-    //override fun onCreateView(
-    //        inflater: LayoutInflater,
-    //        container: ViewGroup?,
-    //        savedInstanceState: Bundle?
-    //): View? {
-    //    //val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
-    //    return inflater.inflate(R.layout.fragment_slideshow, container, false)
-    //}
+    private fun loadSettings() {
+        val sharePreferences = PreferenceManager.getDefaultSharedPreferences(context)
+        val notifications = sharePreferences.getBoolean("switchNotification", true)
+
+        if (notifications){
+            enableNotifications()
+        }
+        else{
+            disableNotifications()
+        }
+    }
+
+    private fun enableNotifications() {
+        val sharedPreferences: SharedPreferences = requireContext().getSharedPreferences(NOTIFICATIONS_PREF, Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("silentMode", false)
+        editor.apply()
+    }
+
+    private fun disableNotifications() {
+        val sharedPreferences: SharedPreferences = requireContext().getSharedPreferences(NOTIFICATIONS_PREF, Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("silentMode", true)
+        editor.apply()
+    }
 }
