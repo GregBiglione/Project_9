ba6584dcf6ded6d55ed3800c2b29133e70307295
GregBiglione <gregory.biglione81@gmail.com>
2021-01-21 02:26:43 +0100

    Added show dialog box to access gallery & camera. And display photo in image view in UpdateHouseFragment

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/fragments/UpdateHouseFragment.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/fragments/UpdateHouseFragment.kt
@@ -1,17 +1,153 @@
 package com.openclassrooms.realestatemanager.ui.fragments
 
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Spinner
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.openclassrooms.realestatemanager.R
+import com.openclassrooms.realestatemanager.adapters.HousePhotoAdapter
+import com.openclassrooms.realestatemanager.database.dao.RealEstateManagerDatabase
+import com.openclassrooms.realestatemanager.injections.ViewModelFactory
+import com.openclassrooms.realestatemanager.model.HousePhoto
+import com.openclassrooms.realestatemanager.repositories.AgentRepository
+import com.openclassrooms.realestatemanager.repositories.HousePhotoRepository
+import com.openclassrooms.realestatemanager.repositories.HouseRepository
+import com.openclassrooms.realestatemanager.ui.dialog_box.PhotoChoiceDialog
+import com.openclassrooms.realestatemanager.utils.ImageConverters
+import com.openclassrooms.realestatemanager.utils.SavePhoto
+import com.openclassrooms.realestatemanager.viewmodel.MainViewModel
 
-class UpdateHouseFragment : Fragment() {
+class UpdateHouseFragment : Fragment(), PhotoChoiceDialog.GalleryListener, PhotoChoiceDialog.CameraListener {
+
+    private lateinit var housePhotoRecyclerView: RecyclerView
+    private lateinit var mainViewModel: MainViewModel
+    private lateinit var housePhotoImageView: ImageView
+    //private var housePhotoList = generateHousePhotoList()
+    //private var housePhotoAdapter = HousePhotoAdapter(housePhotoList)
+    private lateinit var housePhotoDescriptionEditText: TextInputEditText
+    //------------------- Button -------------------------------------------------------------------
+    private lateinit var addHousePhotoButton: Button
+    private lateinit var updateHouseButton: Button
+    //------------------- Text input layout --------------------------------------------------------
+    private lateinit var houseSaleDateInputLyt: TextInputLayout
+    //------------------- Edit text ----------------------------------------------------------------
+    private lateinit var houseDescriptionEditText: TextInputEditText
+    private lateinit var houseAddressEditText: TextInputEditText
+    private lateinit var housePriceEditText: TextInputEditText
+    private lateinit var houseSurfaceEditText: TextInputEditText
+    private lateinit var houseRoomsEditText: TextInputEditText
+    private lateinit var houseBathRoomsEditText: TextInputEditText
+    private lateinit var houseBedRoomsEditText: TextInputEditText
+    private lateinit var houseEntryDate: TextInputEditText
+    private lateinit var houseSaleDate: TextInputEditText
+    //------------------- Spinner ------------------------------------------------------------------
+    private lateinit var houseTypeSpinner: Spinner
+    private lateinit var neighborSpinner: Spinner
+    private lateinit var statusSpinner: Spinner
+    private lateinit var agentsSpinner: Spinner
+    private var selectedAgentId: Long = 0
+    //------------------- Checkbox -----------------------------------------------------------------
+    private lateinit var pointsOfInterests: TextInputEditText
+    private lateinit var listOfPointsOfInterests: Array<String?>
+    private lateinit var checkedPointsOfInterests: BooleanArray
+    private var poi: ArrayList<Int> = ArrayList()
+    //------------------- Uri to bitmap Conversion -------------------------------------------------
+    private lateinit var savePhoto: SavePhoto
+    private lateinit var imageConverters: ImageConverters
+    private var photoFromStorage: Uri? = null
+    private lateinit var housePhotoDescriptionInRecyclerView: String
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update_house, container, false)
+        val view = inflater.inflate(R.layout.fragment_update_house, container, false)
+        //------------------- Photo image view -----------------------------------------------------
+        housePhotoImageView = view.findViewById(R.id.update_add_house_photo)
+        imageConverters = ImageConverters()
+        savePhoto = SavePhoto()
+        clickOnAddHouseImageView()
+        return view
+    }
+
+    //----------------------------------------------------------------------------------------------
+    //------------------- Add house photo from dialog box ------------------------------------------
+    //----------------------------------------------------------------------------------------------
+
+    private fun clickOnAddHouseImageView(){
+        housePhotoImageView.setOnClickListener { showPhotoChoiceDialogBox() }
+    }
+
+    //------------------- Show dialog box ----------------------------------------------------------
+
+    private fun showPhotoChoiceDialogBox(){
+        val photoChoiceDialog = PhotoChoiceDialog(this, this)
+        photoChoiceDialog.show(requireFragmentManager(), "Photo choice dialog box")
+        photoChoiceDialog.setTargetFragment(this, 2)
+    }
+
+    //------------------- Get Uri from dialog box --------------------------------------------------
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    override fun applyGalleryPhoto(uriPhoto: Uri?) {
+        housePhotoImageView.setImageURI(uriPhoto)
+        val bitmap = imageConverters.uriToBitmap(uriPhoto, requireContext())
+        val tempUri: Uri? = savePhoto.getImageUri(requireContext(), bitmap)
+        photoFromStorage = tempUri
+    }
+
+    //------------------- Get Bitmap from dialog box -----------------------------------------------
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    override fun applyCameraPhoto(bitmapPhoto: Bitmap) {
+        housePhotoImageView.setImageBitmap(bitmapPhoto)
+        val tempUri: Uri? = savePhoto.getImageUri(requireContext(), bitmapPhoto)
+        photoFromStorage = tempUri
+    }
+
+    //----------------------------------------------------------------------------------------------
+    //------------------- Add house photo in recyclerview ------------------------------------------
+    //----------------------------------------------------------------------------------------------
+
+    //------------------- Click on add house photo button ------------------------------------------
+
+    //private fun clickOnAddHousePhotoButton(){
+    //    addHousePhotoButton = findViewById(R.id.add_house_add_photo_button)
+    //    addHousePhotoButton.setOnClickListener { addHousePhotoInRecyclerView() }
+    //}
+
+    //private fun generateHousePhotoList(): ArrayList<HousePhoto>{
+    //    return ArrayList()
+    //}
+
+    //private fun addHousePhotoInRecyclerView(){
+//
+    //    val housePhotoDescription: String = housePhotoDescriptionEditText.text.toString().trim()
+//
+    //    val newHousePhoto = HousePhoto(null, photoFromStorage.toString(), housePhotoDescription)
+//
+    //    housePhotoList.add(newHousePhoto)
+    //    housePhotoDescriptionInRecyclerView = housePhotoDescription
+    //    housePhotoAdapter.notifyDataSetChanged()
+    //    clearChamps()
+    //}
+
+    //----------------------------------------------------------------------------------------------
+    //------------------- Fill detail house champs -------------------------------------------------
+    //----------------------------------------------------------------------------------------------
+
+    private fun fillUpdateHouseChamps(){
+
     }
 }
