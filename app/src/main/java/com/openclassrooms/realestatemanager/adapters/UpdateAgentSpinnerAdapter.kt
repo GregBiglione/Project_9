b393f5ba707ec4bd04cfc09953642a1a398cde98
GregBiglione <gregory.biglione81@gmail.com>
2021-01-22 06:40:00 +0100

    Added update agent spinner adapter

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/adapters/UpdateAgentSpinnerAdapter.kt
@@ -0,0 +1,40 @@
+package com.openclassrooms.realestatemanager.adapters
+
+import android.content.Context
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.bumptech.glide.Glide
+import com.openclassrooms.realestatemanager.R
+import com.openclassrooms.realestatemanager.model.Agent
+import kotlinx.android.synthetic.main.spinner_agent_item.view.*
+
+class UpdateAgentSpinnerAdapter(context: Context): ArrayAdapter<Agent>(context, 0) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        return initView(position, convertView, parent)
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+        return initView(position, convertView, parent)
+    }
+
+    private fun initView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val agent = getItem(position)
+        val view = LayoutInflater.from(context).inflate(R.layout.spinner_agent_item, parent, false)
+
+        if (agent != null) {
+            view.spinner_agent_item_image.setImageURI(Uri.parse(agent.agentPhoto))
+        }
+        Glide.with(parent.context)
+                .load(agent?.agentPhoto)
+                .into(view.spinner_agent_item_image)
+        view.spinner_agent_item_name.text = agent?.name
+        view.spinner_agent_item_email.text = agent?.email
+        view.spinner_agent_item_phone.text = agent?.phoneNumber
+        return view
+    }
+}
+
