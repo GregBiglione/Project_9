f0885162fc33585885d6a152b16c56c6efe03879
GregBiglione <gregory.biglione81@gmail.com>
2021-01-26 11:35:38 +0100

    Added a public method in House to transform a content values object into a House object

--- app/src/main/java/com/openclassrooms/realestatemanager/model/House.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/model/House.kt
@@ -1,14 +1,17 @@
 package com.openclassrooms.realestatemanager.model
 
+import android.content.ClipData.Item
+import android.content.ContentValues
 import android.os.Parcelable
 import androidx.room.*
 import com.openclassrooms.realestatemanager.utils.ListConverters
 import kotlinx.android.parcel.Parcelize
 
+
 @Parcelize
 @Entity(foreignKeys = [ForeignKey(entity = Agent::class,
-            parentColumns = ["id"],
-            childColumns = ["agentId"])])
+        parentColumns = ["id"],
+        childColumns = ["agentId"])])
 data class House(
         @PrimaryKey(autoGenerate = true)
         var id: Long? = null,
@@ -29,4 +32,29 @@ data class House(
         var saleDate: Long?,
         @ColumnInfo(name = "agentId", index = true)
         var agentId: Long?
-): Parcelable
+): Parcelable{
+
+        //-------------------------------- Utils ---------------------------------------------------
+
+        fun fromContentValues(values: ContentValues): House {
+                val house = House(id, housePhotoList, typeOfHouse, neighborhood, address, price, surface, numberOfRooms, numberOfBathRooms,
+                        numberOfBedRooms, description, available, proximityPointsOfInterest, entryDate, saleDate, agentId)
+                if (values.containsKey("housePhotoList")) house.housePhotoList
+                if (values.containsKey("typeOfHouse")) house.typeOfHouse
+                if (values.containsKey("neighborhood")) house.neighborhood
+                if (values.containsKey("address")) house.address
+                if (values.containsKey("price")) house.price
+                if (values.containsKey("surface")) house.surface
+                if (values.containsKey("numberOfRooms")) house.numberOfRooms
+                if (values.containsKey("numberOfBathRooms")) house.numberOfBathRooms
+                if (values.containsKey("numberOfBedRooms")) house.numberOfBedRooms
+                if (values.containsKey("description")) house.description
+                if (values.containsKey("available")) house.available
+                if (values.containsKey("proximityPointsOfInterest")) house.proximityPointsOfInterest
+                if (values.containsKey("entryDate")) house.entryDate
+                if (values.containsKey("saleDate")) house.saleDate
+                if (values.containsKey("agentId")) house.agentId
+                return house
+        }
+}
+
