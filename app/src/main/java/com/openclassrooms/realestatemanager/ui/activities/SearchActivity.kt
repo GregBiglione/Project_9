8cec6ba9ae7220f08bbbd8121ae65ea6a4a73eb8
GregBiglione <gregory.biglione81@gmail.com>
2021-01-29 05:34:38 +0100

    Added search button in activity_search & back button in SearchActivity

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/activities/SearchActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/activities/SearchActivity.kt
@@ -2,11 +2,25 @@ package com.openclassrooms.realestatemanager.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.openclassrooms.realestatemanager.R
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    //----------------------------------------------------------------------------------------------
+    //--------------------------------Back button to Main activity ---------------------------------
+    //----------------------------------------------------------------------------------------------
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
