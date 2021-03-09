16917edc572d76a8c51b03ec50314fd9c2092411
GregBiglione <gregory.biglione81@gmail.com>
2021-03-09 17:07:06 +0100

    Added marker on map for each house in MapFragment

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/fragments/MapFragment.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/fragments/MapFragment.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.droidman.ktoasty.showSuccessToast
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
@@ -16,10 +17,14 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.openclassrooms.realestatemanager.R
+import com.openclassrooms.realestatemanager.injections.Injection
+import com.openclassrooms.realestatemanager.injections.ViewModelFactory
+import com.openclassrooms.realestatemanager.viewmodel.MainViewModel
 import com.vmadalin.easypermissions.annotations.AfterPermissionGranted
 import pub.devrel.easypermissions.EasyPermissions
 
@@ -36,10 +41,16 @@ class MapFragment: Fragment() {
     //-------------------------------- Last known location -----------------------------------------
     private lateinit var gps: FloatingActionButton
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    //-------------------------------- View Model --------------------------------------------------
+    private lateinit var mainViewModel: MainViewModel
+    private lateinit var injection: Injection
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_map, container, false)
+        injection = Injection()
+        configureViewModel()
+        addHouseMarker()
         createLocationService()
         gps = view.findViewById(R.id.gps)
         customFocus()
@@ -101,7 +112,7 @@ class MapFragment: Fragment() {
     }
 
     //----------------------------------------------------------------------------------------------
-    //-------------------------------- Get lastKnown location --------------------------------------
+    //-------------------------------- Get last known location -------------------------------------
     //----------------------------------------------------------------------------------------------
 
     //-------------------------------- Location service --------------------------------------------
@@ -118,5 +129,33 @@ class MapFragment: Fragment() {
             map!!.moveCamera(CameraUpdateFactory.newLatLng(littleItaly))
             zoomOnLocation()
         }
+    }
+
+    //----------------------------------------------------------------------------------------------
+    //-------------------------------- Configure ViewModel -----------------------------------------
+    //----------------------------------------------------------------------------------------------
+
+    private fun configureViewModel(){
+        val viewModelFactory: ViewModelFactory = injection.provideViewModelFactory(requireContext())
+        mainViewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
+    }
+
+    //----------------------------------------------------------------------------------------------
+    //-------------------------------- Add marker on map for each house ----------------------------
+    //----------------------------------------------------------------------------------------------
+
+    private fun addHouseMarker(){
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        //------------------- Get houses from room db ----------------------------------------------
+        mainViewModel.allHouses.observe(viewLifecycleOwner, { house ->
+            for (h in house){
+                val houseLat = h.lat
+                val houseLng = h.lng
+                val houseLocation = LatLng(houseLat!!, houseLng!!)
+                map!!.addMarker(MarkerOptions()
+                        .position(houseLocation)
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)))
+            }
+        })
     }
 }
